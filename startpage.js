a6960f535e6309ec145e6a0692200428040501f3
Justus Nickenig <uwkxk@student.kit.edu>
2023-12-29 21:24:49 +0100

    ausgelagert

--- startpage.js
+++ startpage.js
@@ -1,11 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const burgerIcon = document.getElementById('burgerIcon');
+
+function toggleBurgerMenu() {
     const navLinks = document.getElementById('navLinks');
-  
-    burgerIcon.addEventListener('click', function () {
-      navLinks.classList.toggle('show');
-    });
-    burgerIcon.addEventListener('touchend', function () {
-        navLinks.classList.toggle('show');
-      });
-  });
+    navLinks.classList.toggle('show');
+
+}
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     const burgerIcon = document.getElementById('burgerIcon');
+//     const navLinks = document.getElementById('navLinks');
+
+//     burgerIcon.addEventListener('click', function () {
+//         navLinks.classList.toggle('show');
+//     });
+//     burgerIcon.addEventListener('touchend', function () {
+//         navLinks.classList.toggle('show');
+//     });
+// });
